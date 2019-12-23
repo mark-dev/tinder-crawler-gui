dc9569a50c33519aa33ed5382bfc1912f7f4f425
Mark <markdev32@gmail.com>
2019-12-23 23:01:56 +0300

    sql view refactoring

--- sql/scripts/schema.sql
+++ sql/scripts/schema.sql
@@ -50,6 +50,9 @@ create index if not exists crawler_data_enrich_required_index
 create index if not exists crawler_data_height_index
     on crawler_data (height);
 
+create index if not exists idx_crawler_data_bio_length
+    ON crawler_data (length(bio));
+
 create index if not exists crawler_data_updated_at_index
     on crawler_data (updated_at);
 
@@ -62,38 +65,54 @@ CREATE index trgm_idx_crawler_data_bio
     ON crawler_data
         USING gin (lower(bio) gin_trgm_ops);
 
+CREATE OR REPLACE VIEW public.tcrawler_no_verdict AS
+SELECT * FROM crawler_data
+WHERE crawler_data.hidden = false AND (crawler_data.verdict = 0 OR (crawler_data.verdict = 2 AND crawler_data.autoliked));
+
 
 CREATE VIEW tcrawler_top AS
-SELECT * FROM crawler_data
-WHERE hidden = FALSE AND verdict = 0 AND verdict_sync_at is null
+SELECT * FROM tcrawler_no_verdict
 ORDER BY rating desc, updated_at DESC, id ASC;
 
 
 CREATE VIEW tcrawler_search AS
-SELECT * FROM crawler_data
-WHERE hidden = FALSE AND verdict = 0 AND verdict_sync_at is null
+SELECT * FROM tcrawler_no_verdict
 order by updated_at DESC, id ASC;
 
 
 CREATE VIEW tcrawler_latest AS
-select * from crawler_data
-WHERE rating > 0 AND hidden = FALSE AND verdict = 0 AND verdict_sync_at is null
+select * from tcrawler_no_verdict
+WHERE rating > 0
 order by date_trunc('day', GREATEST(ts,updated_at)) DESC, rating DESC, length(bio) DESC, id ASC;
 
 
 CREATE VIEW tcrawler_random AS
-select * from crawler_data where hidden = FALSE and now() - updated_at <= interval '7 days' and height > 0 and height < 170 and verdict = 0 order by random();
+select * from tcrawler_no_verdict
+where now() - updated_at <= interval '7 days'
+and height between 150 and 170
+order by random();
 
 CREATE VIEW tcrawler_today AS
-select * from crawler_data where date_trunc('day', ts) = date_trunc('day',now()) and verdict = 0 order by rating desc;
+select * from tcrawler_no_verdict
+where date_trunc('day', ts) = date_trunc('day',now())
+order by rating desc;
 
 CREATE VIEW tcrawler_possible_likes AS
-select * from crawler_data where verdict = 0 AND verdict_sync_at is null AND now() - updated_at <= interval '7 days'  AND avg_batch_rank_idx  > 40 order by avg_batch_rank_idx DESC, id ASC;
+select * from tcrawler_no_verdict
+where now() - updated_at <= interval '7 days'  AND avg_batch_rank_idx  > 40
+ order by avg_batch_rank_idx DESC, id ASC;
 
 CREATE VIEW tcrawler_autolike AS
-select * from crawler_data where height between 150 and 170 and rating > 0 and verdict = 0 order by updated_at asc,id ASC;
+select * from tcrawler_no_verdict
+where height between 150 and 170 and rating > 0
+order by updated_at asc,id ASC;
 
 CREATE VIEW tcrawler_autodislike AS
-select * from crawler_data where length(bio) = 0 and avg_batch_rank_idx < 5 and verdict = 0 order by updated_at asc,id ASC;
+select * from tcrawler_no_verdict
+where length(bio) = 0 and avg_batch_rank_idx < 5
+order by updated_at asc,id ASC;
 
-CREATE VIEW tcrawler_near AS select * from crawler_data where date_trunc('day',now())=date_trunc('day', updated_at) and verdict = 0 and distance = 1 order by rating desc;
+CREATE VIEW tcrawler_near AS
+select * from tcrawler_no_verdict where date_trunc('day',now())=date_trunc('day', updated_at)
+and distance = 1
+order by rating desc;
